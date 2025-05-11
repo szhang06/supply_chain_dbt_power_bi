65d8b9642a17b0394fbccb4c7915dce6cfb6242d
wenwen <wenwen.s.zhang@gmail.com>
2025-05-11 17:20:20 +0000

    full files

---
+++ models/marts/business/shipment_performance.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+WITH shipments AS (
+    SELECT
+        s.*,
+        d.year,
+        d.month,
+        d.day_of_week
+    FROM {{ ref('fact_shipments') }} s
+    LEFT JOIN {{ ref('dim_time') }} d ON s.date = d.date
+)
+
+SELECT
+    asset_id,
+    year,
+    month,
+    day_of_week,
+    COUNT(*) AS total_shipments,
+    SUM(CASE WHEN is_delayed = '1' THEN 1 ELSE 0 END) AS delayed_shipments,
+    AVG(waiting_time_minutes) AS avg_waiting_time,
+    AVG(temperature) AS avg_temperature,
+    AVG(humidity) AS avg_humidity,
+    delay_reason,
+    traffic_status
+FROM shipments
+GROUP BY asset_id, year, month, day_of_week, delay_reason, traffic_status
---
+++ models/marts/core/fact_shipments.sql
@@ -0,0 +1,32 @@
+{{
+  config(
+    materialized='table',
+    unique_key='shipment_id'
+  )
+}}
+
+WITH stg_data AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (ORDER BY timestamp) AS shipment_id
+    FROM {{ ref('stg_smart_logistics') }}
+)
+
+SELECT
+    shipment_id,
+    asset_id,
+    timestamp,
+    DATE(timestamp) as date,
+    shipment_status,
+    is_delayed,
+    delay_reason,
+    waiting_time_minutes,
+    traffic_status,
+    temperature,
+    humidity,
+    inventory_level,
+    user_transaction_amount,
+    user_purchase_frequency,
+    asset_utilization_percentage,
+    demand_forecast
+FROM stg_data
